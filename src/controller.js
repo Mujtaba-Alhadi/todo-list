68b48e1b8f43ab23ef7b381d63aa0d93fc85bb59
mujtaba-alhadi <mujtabaalhadi22@gmail.com>
2025-08-27 02:58:36 +0300

    added controller logic

---
+++ src/controller.js
@@ -0,0 +1,48 @@
+class Task {
+  constructor(id, title, description, dueDate, priority) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+class Project {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    this.taskArr = [];
+  }
+
+  addTask(task) {
+    this.taskArr.push(task);
+  }
+
+  removeTask(task) {
+    const index = this.taskArr.findIndex((todo) => todo.id === task.id);
+    if (index !== -1) {
+      this.taskArr.splice(index, 1);
+    }
+  }
+}
+
+const controller = (function () {
+  let projectsArr = [];
+
+  const createTodo = (title, description, dueDate, priority) => {
+    return new Task(crypto.randomUUID(), title, description, dueDate, priority);
+  };
+
+  const createProject = (name) => {
+    const newProject = new Project(name, crypto.randomUUID());
+    projectsArr.push(newProject);
+    return newProject;
+  };
+
+  const getProjectArr = () => projectsArr;
+
+  return { createTodo, createProject, getProjectArr };
+})();
+
+export default controller;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import "./style.css";
-console.log("Yo");
+import controller from "./controller.js";
+
+// todo class
+// projects = separate list of todos
+// default project, users can create new projects
+// separate the logic from the DOM (new todos, setting todos as complete, changing priority)
+// the ui should be able to:
+//    view all projects
+//    view all todos in each project (without the description)
+//    expand a todo to see/edit it
+//    delete a todo
+// for formatting and manipulating dates use date-fns library
+// function that uses localStorage to save data
+// function that looks for that data in localStorage when the app is loaded
+// make sure it doesn't crash if the data is not there
+// figure out how to add methods back to your object properties once you fetch them
+
+
+const myProject = controller.createProject("myProject");
+const task1 = controller.createTodo("Task 1", "Description 1", "2025-09-01", "High");
+const task2 = controller.createTodo("Task 2", "Description 2", "2025-09-05", "Low");
+myProject.addTask(task1);
+myProject.addTask(task2);
+myProject.removeTask(task1);
+console.log(controller.getProjectArr());
